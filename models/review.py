4b79f71dd2b8b57ddb72ca8c8d9ae4965ca0a47f
Mado007 <mahmodeid2019@gmail.com>
2023-09-20 06:16:34 +0300

    Fix Excute

--- models/review.py
+++ models/review.py
@@ -1,10 +1,31 @@
-#!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" holds class Review"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+import models
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Representation of Review 
+    Attributes:
+        __tablename__: table name
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    if models.storage_type == "db":
+        __tablename__ = "reviews"
+
+        place_id = Column(String(60),
+                          ForeignKey("places.id"))
+        user_id = Column(String(60),
+                         ForeignKey("users.id"))
+        text = Column(String(1024),
+                      nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+""" holds class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
+class User(BaseModel, Base):
+    """Representation of a user """
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="delete")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
