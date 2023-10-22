9e96660a0bbeaf7d7af239747bb5e6a7330ef911
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-22 12:36:51 +0300

    routing

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
+"""nodule documentation for 0-routing in flask"""
+
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = True
+app.url_map.strict_slashes = False
 
 @app.route("/")
 def index():
     return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run()
