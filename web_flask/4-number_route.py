32434239dbd719fca0eb47aee4204bbb555b1b68
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-22 18:12:26 +0300

    typed path variable

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,63 @@
 #!/usr/bin/python3
+""" Starts a Flash Web Application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """index page for my application
+
+    Returns:
+        string: to be viewed on browser
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb page for my application
+
+    Returns:
+        string: to be viewed on browser
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_variable(text):
+    """/c/<text> page for my application
+
+    Returns:
+        string: to be viewed on browser
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_variable(text):
+    """/python/<text> page for my application
+
+    Returns:
+        string: to be viewed on browser
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_variable(n):
+    """/number/<n> page for my application
+
+    Returns:
+        string: to be viewed on browser
+    """
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    """to prevent from running when imported
+    """
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
